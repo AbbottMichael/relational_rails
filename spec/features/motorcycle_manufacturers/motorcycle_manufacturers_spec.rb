9ec3a6be9be2ed9061f45814d0c70d1bb1311d2d
Michael Abbott <michael@mjabbottdesign.com>
2021-07-07 17:48:14 -0700

    Add motorcycle manufactures story 2 test

--- spec/features/motorcycle_manufacturers/motorcycle_manufacturers_spec.rb
+++ spec/features/motorcycle_manufacturers/motorcycle_manufacturers_spec.rb
@@ -15,10 +15,20 @@ RSpec.describe 'view motorcycle manufacturers page' do
     expect(page).to have_content(harley_davidson.name)
   end
 
-  it '(user story 2)' do
+  it "displays the motorcycle manufacturer and it's attributes by id (user story 2)" do
 #     As a visitor
 #     When I visit '/motorcycle_manufacturers/:id'
 #     Then I see the motorcycle manufacturer with that id including the motorcycle manufacturer's attributes:
 #     - data from each column that is on the motorcycle_manufacturers table
+    zero = MotorcycleManufacturer.create!(name: 'Zero', headquarters: 'Santa Cruz, California', usa_made: true, total_models_all_time: 29)
+    harley_davidson = MotorcycleManufacturer.create!(name: 'Harley Davidson', headquarters: 'Milwaukee, Wisconsin', usa_made: true, total_models_all_time: 1_615)
+
+    visit "/motorcycle_manufacturers/#{zero.id}"
+    save_and_open_page
+    expect(page).to have_content(zero.name)
+    expect(page).to have_content("Headquarters: #{zero.headquarters}")
+    expect(page).to have_content("USA made?: #{zero.usa_made}")
+    expect(page).to have_content("Created at: #{zero.created_at}")
+    expect(page).to have_content("Updated at: #{zero.updated_at}")
   end
 end
