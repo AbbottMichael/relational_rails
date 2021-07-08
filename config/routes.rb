1228d832915370b8479632494b64b0376eb39ac7
Michael Abbott <michael@mjabbottdesign.com>
2021-07-07 19:43:39 -0700

    Complete story 3 motorcycles

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get '/game_studios/:id', to: "game_studios#show"
 
   get '/motorcycle_manufacturers',     to: 'motorcycle_manufacturers#index'
-  get '/motorcycle_manufacturers/:id', to: 'motorcycle_manufacturers#show' 
+  get '/motorcycle_manufacturers/:id', to: 'motorcycle_manufacturers#show'
+
+  get '/motorcycles', to: 'motorcycles#index'
 end
