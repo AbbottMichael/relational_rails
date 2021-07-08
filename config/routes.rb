4628e81ac405c5d163f38ed1ded69437f90c4164
Michael Abbott <michael@mjabbottdesign.com>
2021-07-07 17:49:30 -0700

    Add motorcycle manufacturers story 2 route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  
+
   get '/game_studios', to: 'game_studios#index'
-  get '/motorcycle_manufacturers', to: 'motorcycle_manufacturers#index'
+
+  get '/motorcycle_manufacturers',     to: 'motorcycle_manufacturers#index'
+  get '/motorcycle_manufacturers/:id', to: 'motorcycle_manufacturers#show' 
 end
