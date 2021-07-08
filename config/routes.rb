4873359297e880097e815644502e9450d0d141ee
Jacob Martinez <jacobmarcareer@gmail.com>
2021-07-07 18:47:09 -0600

    Complete user story 2 for Games Studio table

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
   get '/game_studios', to: 'game_studios#index'
+  get '/game_studios/:id', to: "game_studios#show"
+
   get '/motorcycle_manufacturers', to: 'motorcycle_manufacturers#index'
 end
