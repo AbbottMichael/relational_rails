8ff933833bfb8635cfcc3291ece57d1e4f029c86
Michael Abbott <michael@mjabbottdesign.com>
2021-07-08 19:32:25 -0700

    User story 5 complete; add should-matchers gem

--- app/models/motorcycle_manufacturer.rb
+++ app/models/motorcycle_manufacturer.rb
@@ -1,3 +1,8 @@
 class MotorcycleManufacturer < ApplicationRecord
   has_many :motorcycles
+
+  validates_presence_of :name,
+                        :headquarters,
+                        :usa_made,
+                        :total_models_all_time
 end
