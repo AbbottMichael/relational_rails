a3baac429d45ac078a1a1e88552cc9c8b31f9a67
Michael Abbott <michael@mjabbottdesign.com>
2021-07-09 19:40:55 -0700

    Complete user story 7

--- spec/features/motorcycle_manufacturers/show_spec.rb
+++ spec/features/motorcycle_manufacturers/show_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe 'view motorcycle manufacturer by ID page' do
   before :each do
     @zero = MotorcycleManufacturer.create!(name: 'Zero', headquarters: 'Santa Cruz, California', usa_made: true, total_models_all_time: 29)
     @harley_davidson = MotorcycleManufacturer.create!(name: 'Harley Davidson', headquarters: 'Milwaukee, Wisconsin', usa_made: true, total_models_all_time: 1_615)
+    @srs = Motorcycle.create!(model: 'SR/S', price: 19_995.0, electric: true, motorcycle_manufacturer_id: @zero.id)
   end
 
   it "displays the motorcycle manufacturer and it's attributes by id (user story 2)" do
@@ -12,11 +13,20 @@ RSpec.describe 'view motorcycle manufacturer by ID page' do
     #  Then I see the motorcycle manufacturer with that id including the motorcycle manufacturer's attributes:
     #  - data from each column that is on the motorcycle_manufacturers table
     visit "/motorcycle_manufacturers/#{@zero.id}"
-    save_and_open_page
+
     expect(page).to have_content(@zero.name)
     expect(page).to have_content("Headquarters: #{@zero.headquarters}")
     expect(page).to have_content("USA made?: #{@zero.usa_made}")
     expect(page).to have_content("Created at: #{@zero.created_at}")
     expect(page).to have_content("Updated at: #{@zero.updated_at}")
+  end
+
+  it "displays the motorcycle manufacturer's motorcycle count (user story 7)" do
+    #  As a visitor
+    #  When I visit motorcycle manufacturer's show page
+    #  I see a count of the number of motorcycles associated with this motorcycle manufacturer
+    visit "/motorcycle_manufacturers/#{@zero.id}"
+
+    expect(page).to have_content("Motorcycles: #{@zero.motorcycles.count}")
   end
 end
