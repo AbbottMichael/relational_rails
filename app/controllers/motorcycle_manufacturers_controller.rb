a5333d27b1cf29d6de2fe8829f0b38cf66ad1d8c
Michael Abbott <michael@mjabbottdesign.com>
2021-07-10 16:48:31 -0700

    Refactor to include params.permit

--- app/controllers/motorcycle_manufacturers_controller.rb
+++ app/controllers/motorcycle_manufacturers_controller.rb
@@ -11,12 +11,11 @@ class MotorcycleManufacturersController < ApplicationController
   end
 
   def create
-    MotorcycleManufacturer.create(
-      name:                  params[:name],
-      headquarters:          params[:headquarters],
-      usa_made:              params[:usa_made],
-      total_models_all_time: params[:total_models_all_time]
-    )
+    MotorcycleManufacturer.create(motorcycle_manufacturer_params)
     redirect_to "/motorcycle_manufacturers"
+  end
+
+  def motorcycle_manufacturer_params
+    params.permit(:name, :headquarters, :usa_made, :total_models_all_time)
   end
 end
