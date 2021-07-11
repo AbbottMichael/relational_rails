f7629d83e4c2670b1240ba50ef54a712d0052493
Jacob Martinez <jacobmarcareer@gmail.com>
2021-07-11 11:42:58 -0600

    Update models to validate data entered

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,9 @@
 class Game < ApplicationRecord
   belongs_to :game_studio
+
+  validates_presence_of :game_name,
+                        :game_cost,
+                        :genre
+
+  validates             :multiplatform, inclusion: [true, false]
 end
--- app/models/game_studio.rb
+++ app/models/game_studio.rb
@@ -1,3 +1,8 @@
 class GameStudio < ApplicationRecord
   has_many :games
+
+  validates_presence_of :studio_name,
+                        :employee_count
+
+  validates             :hiring, inclusion: [true, false]
 end
