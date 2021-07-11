f1eeac1b81b9fb1d1da02d33b50e1072f5f776c1
Jacob Martinez <jacobmarcareer@gmail.com>
2021-07-10 18:58:27 -0600

    Complete user story 12 for games tables

--- app/controllers/game_studios_controller.rb
+++ app/controllers/game_studios_controller.rb
@@ -11,9 +11,21 @@ class GameStudiosController < ApplicationController
   end
 
   def create
-    game_studio = GameStudio.create(game_studio_params)
+    GameStudio.create(game_studio_params)
     redirect_to '/game_studios'
   end
+
+  def edit
+    @game_studio = GameStudio.find(params[:id])
+  end
+
+  def update
+    game_studio = GameStudio.find(params[:id])
+    game_studio.update(game_studio_params)
+    redirect_to "/game_studios/#{game_studio.id}"
+  end
+
+  private
 
   def game_studio_params
     params.permit(:studio_name, :employee_count, :hiring)
---
+++ spec/features/game_studios/edit_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe 'the game studio edit' do
+  it 'links to the edit page' do
+    game_studio = GameStudio.create!(
+      studio_name: 'Konami',
+      employee_count: 10_454,
+      hiring: true
+    )
+    
+    visit "/game_studios/#{game_studio.id}"
+
+    click_button "Update #{game_studio.studio_name}"
+
+    expect(current_path).to eq("/game_studios/#{game_studio.id}/edit")
+  end
+
+  it 'can edit the game studio' do
+    game_studio = GameStudio.create!(
+      studio_name: 'Koname',
+      employee_count: 10_454,
+      hiring: true
+    )
+
+    visit "/game_studios/#{game_studio.id}"
+
+    expect(page).to have_content('Koname')
+
+    click_button "Update Koname"
+
+    fill_in('Studio Name', with: 'Konami')
+    fill_in('Employee count', with: 10_454)
+    choose('True')
+    click_button('Update Game Studio')
+
+    expect(current_path).to eq("/game_studios/#{game_studio.id}")
+    expect(page).to have_content('Konami')
+  end
+end
