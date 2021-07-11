5efc0c7f6481c85f114d96f2bd2ef3043ec7b279
Michael Abbott <michael@mjabbottdesign.com>
2021-07-11 16:01:33 -0700

    Complete user story 14

--- app/controllers/motorcycles_controller.rb
+++ app/controllers/motorcycles_controller.rb
@@ -7,4 +7,18 @@ class MotorcyclesController < ApplicationController
   def show
     @motorcycle = Motorcycle.find(params[:id])
   end
+
+  def edit
+    @motorcycle = Motorcycle.find(params[:id])
+  end
+
+  def update
+    motorcycle = Motorcycle.find(params[:id])
+    motorcycle.update(motorcycle_params)
+    redirect_to "/motorcycles/#{motorcycle.id}"
+  end
+
+  def motorcycle_params
+    params.permit(:model, :price, :electric)
+  end
 end
