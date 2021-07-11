0306f635d1021cc38e1f079d7273e608fbf5d070
Michael Abbott <michael@mjabbottdesign.com>
2021-07-11 13:59:20 -0700

    Complete user story 13

--- app/controllers/motorcycle_manufacturer_motorcycles_controller.rb
+++ app/controllers/motorcycle_manufacturer_motorcycles_controller.rb
@@ -3,4 +3,18 @@ class MotorcycleManufacturerMotorcyclesController < ApplicationController
     @motorcycle_manufacturer = MotorcycleManufacturer.find(params[:id])
     @motorcycles = @motorcycle_manufacturer.motorcycles
   end
+
+  def new
+    @moto_mft = MotorcycleManufacturer.find(params[:id])
+  end
+
+  def create
+    @moto_mft = MotorcycleManufacturer.find(params[:id])
+    Motorcycle.create(motorcycle_params)
+    redirect_to "/motorcycle_manufacturers/#{@moto_mft.id}/motorcycles"
+  end
+
+  def motorcycle_params
+    params.permit(:model, :price, :electric, :motorcycle_manufacturer_id)
+  end
 end
