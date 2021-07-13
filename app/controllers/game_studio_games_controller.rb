4d7ca9f27b7e127056a545361eed37990d035d28
Jacob Martinez <jacobmarcareer@gmail.com>
2021-07-13 13:27:39 -0600

    Dry out code with model methods

--- app/controllers/game_studio_games_controller.rb
+++ app/controllers/game_studio_games_controller.rb
@@ -3,9 +3,9 @@ class GameStudioGamesController < ApplicationController
     @game_studio = GameStudio.find(params[:id])
 
     if params[:order] == 'alphabetical'
-      @games = Game.alphabetical(@game_studio.id)
+      @games = Game.sort_by_name(@game_studio.id)
     elsif params[:order] == 'cost'
-      @games = Game.where(game_studio_id: @game_studio.id).order(:game_cost)
+      @games = Game.sort_by_cost(@game_studio.id)
     else
       @games = Game.where(game_studio_id: @game_studio.id)
     end
