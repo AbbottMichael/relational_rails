a290ec46839b9a678bc1bc7076be488e580578fc
Jacob Martinez <jacobmarcareer@gmail.com>
2021-07-12 20:39:33 -0600

    Make tests for deleting game studios

--- spec/features/game_studios/index_spec.rb
+++ spec/features/game_studios/index_spec.rb
@@ -2,9 +2,56 @@ require 'rails_helper'
 
 RSpec.describe 'view game studios index page' do
   before :each do
-    @nintendo = GameStudio.create!(studio_name: 'Nintendo', employee_count: 6_547, hiring: true)
-    @sony = GameStudio.create!(studio_name: 'Sony', employee_count: 5_000, hiring: false)
-    @sega = GameStudio.create!(studio_name: 'Sega', employee_count: 3_238, hiring: true)
+    @nintendo = GameStudio.create!(
+      studio_name: 'Nintendo',
+      employee_count: 6_547,
+      hiring: true
+    )
+    @sony = GameStudio.create!(
+      studio_name: 'Sony',
+      employee_count: 5_000,
+      hiring: true
+    )
+    @sega = GameStudio.create!(
+      studio_name: 'Sega',
+      employee_count: 3_238,
+      hiring: true
+    )
+    @super_mario = Game.create!(
+      game_name: 'Super Mario World',
+      game_cost: 40.99,
+      genre: 'platformer',
+      multiplatform: true,
+      game_studio_id: @nintendo.id
+    )
+    @kirby = Game.create!(
+      game_name: 'Kirby',
+      game_cost: 57.99,
+      genre: 'platformer',
+      multiplatform: true,
+      game_studio_id: @nintendo.id
+    )
+    @zelda = Game.create!(
+      game_name: 'Zelda',
+      game_cost: 54.99,
+      genre: 'action-adventure',
+      multiplatform: true,
+      game_studio_id: @nintendo.id
+    )
+    @donkey_kong = Game.create!(
+      game_name: 'Donkey Kong',
+      game_cost: 49.99,
+      genre: 'platformer',
+      multiplatform: true,
+      game_studio_id: @nintendo.id
+    )
+    @spyro = Game.create!(
+      game_name: 'Spyro the Dragon',
+      game_cost: 49.99,
+      genre: 'platformer',
+      multiplatform: true,
+      game_studio_id: @sony.id
+    )
   end
 
   it 'displays all game studios(user story 1)' do
@@ -36,5 +83,36 @@ RSpec.describe 'view game studios index page' do
     click_link('Edit Nintendo')
 
     expect(current_path).to eq("/game_studios/#{@nintendo.id}/edit")
+  end
+
+  it 'has a button to delete the game studio(user story 22)' do
+    visit '/game_studios'
+
+    expect(page).to have_link('Delete Nintendo')
+  end
+
+  it 'can delete the game studio and return to game studio index(user story 22)' do
+    visit '/game_studios'
+
+    click_link('Delete Nintendo')
+
+    expect(current_path).to eq('/game_studios')
+    expect(page).to_not have_content('Nintendo')
+  end
+
+  it 'can confirm that children are deleted(user story 22)' do
+    visit '/game_studios'
+
+    click_link('Delete Nintendo')
+
+    expect(current_path).to eq('/game_studios')
+    
+    visit '/games'
+
+    expect(page).to have_content(@spyro.game_name)
+    expect(page).to_not have_content(@super_mario.game_name)
+    expect(page).to_not have_content(@donkey_kong.game_name)
+    expect(page).to_not have_content(@zelda.game_name)
+    expect(page).to_not have_content(@kirby.game_name)
   end
 end
