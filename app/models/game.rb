60e3561289a216e32111216d397bd63a4f722f24
Jacob Martinez <jacobmarcareer@gmail.com>
2021-07-13 13:27:24 -0600

    Add new method and rename for clarity

--- app/models/game.rb
+++ app/models/game.rb
@@ -7,7 +7,11 @@ class Game < ApplicationRecord
 
   validates             :multiplatform, inclusion: [true, false]
 
-  def self.alphabetical(id)
+  def self.sort_by_name(id)
     where(game_studio_id: id).order(Arel.sql("lower(game_name)"))
+  end
+
+  def self.sort_by_cost(id)
+    where(game_studio_id: id).order(:game_cost)
   end
 end
