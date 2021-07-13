1c208bf6de5642d4b8a48d46e66fc5fea151af6a
Jacob Martinez <jacobmarcareer@gmail.com>
2021-07-13 00:05:14 -0600

    Change routes for game studios to resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
 
-  get '/game_studios',          to: 'game_studios#index'
-  get '/game_studios/new',      to: 'game_studios#new'
-  post '/game_studios',         to: 'game_studios#create'
-  get '/game_studios/:id',      to: 'game_studios#show'
-  get '/game_studios/:id/edit', to: 'game_studios#edit'
-  patch '/game_studios/:id',    to: 'game_studios#update'
-  delete '/game_studios/:id',   to: 'game_studios#destroy'
+  # get '/game_studios',          to: 'game_studios#index'
+  # get '/game_studios/new',      to: 'game_studios#new'
+  # post '/game_studios',         to: 'game_studios#create'
+  # get '/game_studios/:id',      to: 'game_studios#show'
+  # get '/game_studios/:id/edit', to: 'game_studios#edit'
+  # patch '/game_studios/:id',    to: 'game_studios#update'
+  # delete '/game_studios/:id',   to: 'game_studios#destroy'
+  resources :game_studios
 
   get '/game_studios/:id/games',     to: 'game_studio_games#index'
   get '/game_studios/:id/games/new', to: 'game_studio_games#new'
